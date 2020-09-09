f96a41eb1d90f4596faeb3d018a9e4292a428a93
Bethwelkip <bkiplimo@princeton.edu>
2020-09-09 17:22:36 -0400

    add jquery functions

--- js/app.js
+++ js/app.js
@@ -1,34 +1,67 @@
-function size() {
+function small() {
 
 
 };
 
-function toppings() {
+function medium() {
 
 
 };
 
-function crust() {
+window.onload = function large() {
+    if (document.getElementById('isAgeSelected').checked) {
+        $("#txtAge").show();
+        $("#txtAge").show();
+        $("#txtAge").hide();
+        $("#txtAge").hide();
+    } else if (document.getElementById('isAgeSelected').checked) {
+        $("#txtAge").hide();
+        $("#txtAge").show();
+        $("#txtAge").hide();
+    }
+    else {
+        $("#txtAge").hide();
+        $("#txtAge").show();
+        $("#txtAge").hide();
+    }
+    // crusts
+    document.getElementById("crusty-cost").innerHTML = "$1.50";
+    /*document.getElementById("thin").innerHTML = "$" + 1.50;
+    document.getElementById("hard").innerHTML = "$" + 1.50;
+    document.getElementById("thick").innerHTML = "$" + 1.50;
+    toppings
+    document.getElementById("total-charge").innerHTML = ;
+    document.getElementById("total-charge").innerHTML = ;
+    document.getElementById("total-charge").innerHTML = ;
+    document.getElementById("total-charge").innerHTML = ;
+    document.getElementById("total-charge").innerHTML = ;
+    document.getElementById("total-charge").innerHTML = ; */
 
 
 };
 
-function pizza(size = "", topping = "", crust = "", delivery = 0) {
-    this.size = size;
-    this.topping = topping;
-    this.crust = crust;
-    this.delivery = delivery;
-    this.charge = 100; /*crust() + toppings() + size(); */
-};
+function Pizza() {
+    this.size = "";
+    this.topping = "";
+    this.crust = "";
+    this.delivery = "";
+    this.charge = 1000;
+}
+/*var sized = document.getElementById("total-charge").value;
+var toped = document.getElementById("total-charge").value;
+var crusted = document.getElementById("total-charge").value;
+var delivered = document.getElementById("total-charge").value; */
+var myPizza = new Pizza();
 
 function finalPrice() {
-    document.getElementById("total-charge").append(pizza(2, 3, 4, 5).charge);
-    return 2000000;
-}
-
+    document.getElementById("total-charge").innerHTML = "$" + myPizza.charge;
+};
 $(document).ready(function () {
-    $().click(function () {
+    $(".large").click(function () {
+        document.getElementById("crusty-cost").innerHTML = "$1.50";
 
-    })
+
+    });
+
 
 });
