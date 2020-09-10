72cfa5c8c587d0ca9f8fe15f4dd2fc29473c3c83
Bethwelkip <bkiplimo@princeton.edu>
2020-09-10 04:11:14 -0400

    add functions to get pizza sizes

--- js/app.js
+++ js/app.js
@@ -1,67 +1,63 @@
 function small() {
-
-
+    myPizza.size = "Small";
+    myPizza.charge += 5;
+    console.log(myPizza.size);
 };
 
 function medium() {
-
-
+    myPizza.size = "Medium";
+    myPizza.charge += 7;
+    console.log(myPizza.size);
 };
 
-window.onload = function large() {
-    if (document.getElementById('isAgeSelected').checked) {
-        $("#txtAge").show();
-        $("#txtAge").show();
-        $("#txtAge").hide();
-        $("#txtAge").hide();
-    } else if (document.getElementById('isAgeSelected').checked) {
-        $("#txtAge").hide();
-        $("#txtAge").show();
-        $("#txtAge").hide();
+function large() {
+    myPizza.size = "Large";
+    myPizza.charge += 9;
+    console.log(myPizza.charge);
+}
+
+function crust() {
+    console.log(myPizza.charge);
+    if (myPizza.size === "Large") {
+        document.getElementById("crusty-cost").innerHTML = "$2.50";
+        document.getElementById("thin-crust").innerHTML = "$" + 2.50;
+        document.getElementById("hard-crust").innerHTML = "$" + 2.50;
+        document.getElementById("thick-crust").innerHTML = "$" + 2.50;
+        document.getElementById("italian-crust").innerHTML = "$" + 2.50;
+
+    }
+    else if (myPizza.size === "Medium") {
+        document.getElementById("crusty-cost").innerHTML = "$2.00";
+        document.getElementById("thin-crust").innerHTML = "$" + 2.00;
+        document.getElementById("hard-crust").innerHTML = "$" + 2.00;
+        document.getElementById("thick-crust").innerHTML = "$" + 2.00;
+        document.getElementById("italian-crust").innerHTML = "$" + 2.00;
     }
     else {
-        $("#txtAge").hide();
-        $("#txtAge").show();
-        $("#txtAge").hide();
+        document.getElementById("crusty-cost").innerHTML = "$1.50";
+        document.getElementById("thin-crust").innerHTML = "$" + 1.50;
+        document.getElementById("hard-crust").innerHTML = "$" + 1.50;
+        document.getElementById("thick-crust").innerHTML = "$" + 1.50;
+        document.getElementById("italian-crust").innerHTML = "$" + 1.50;
+
     }
-    // crusts
-    document.getElementById("crusty-cost").innerHTML = "$1.50";
-    /*document.getElementById("thin").innerHTML = "$" + 1.50;
-    document.getElementById("hard").innerHTML = "$" + 1.50;
-    document.getElementById("thick").innerHTML = "$" + 1.50;
-    toppings
-    document.getElementById("total-charge").innerHTML = ;
-    document.getElementById("total-charge").innerHTML = ;
-    document.getElementById("total-charge").innerHTML = ;
-    document.getElementById("total-charge").innerHTML = ;
-    document.getElementById("total-charge").innerHTML = ;
-    document.getElementById("total-charge").innerHTML = ; */
 
-
-};
+}
 
 function Pizza() {
     this.size = "";
     this.topping = "";
     this.crust = "";
     this.delivery = "";
-    this.charge = 1000;
+    this.charge = 0;
 }
-/*var sized = document.getElementById("total-charge").value;
-var toped = document.getElementById("total-charge").value;
-var crusted = document.getElementById("total-charge").value;
-var delivered = document.getElementById("total-charge").value; */
+
 var myPizza = new Pizza();
 
 function finalPrice() {
+    document.getElementById("size-price").innerHTML = "$" + myPizza.charge;
+    document.getElementById("crust-price").innerHTML = "$" + myPizza.charge;
+    document.getElementById("topping-price").innerHTML = "$" + myPizza.charge;
+    document.getElementById("delivery-price").innerHTML = "$" + myPizza.charge;
     document.getElementById("total-charge").innerHTML = "$" + myPizza.charge;
 };
-$(document).ready(function () {
-    $(".large").click(function () {
-        document.getElementById("crusty-cost").innerHTML = "$1.50";
-
-
-    });
-
-
-});
